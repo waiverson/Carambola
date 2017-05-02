0f0857e319016826ce5afb52c3685539344ce190
waiverson <237476161@qq.com>
2017-05-02 14:22:36 +0800

    add support for let keyword

---
+++ src/main/java/com/github/waiverson/carambola/StatementExecutorConsumer.java
@@ -0,0 +1,11 @@
+package com.github.waiverson.carambola;
+
+import com.github.waiverson.carambola.support.Variables;
+
+/**
+ * Created by waiverson on 2016/8/3.
+ */
+
+public interface StatementExecutorConsumer {
+    void setStatementExecutor(StatementExecutorInterface var1);
+}
